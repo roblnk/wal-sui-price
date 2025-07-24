7fc63fb48c01a0cd6354cfb2de19aa2da6b48383
Vu Khang <vunguyentrungkhang@gmail.com>
2025-07-24 12:25:01 +0700

    check

--- .next/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_7fc6eb41._.js",
-  "static/chunks/node_modules_6c0ec2f8._.js"
-],
-    source: "dynamic"
-});
